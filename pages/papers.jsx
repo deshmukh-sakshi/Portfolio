2d6d515d9d66898103897a9a5dcb77090119309d
Sakshi-86 <160449887+Sakshi-86@users.noreply.github.com>
2024-12-09 09:24:51 +0530

    Final change

---
+++ pages/papers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Papers = () => {
+  return (
+    <div>
+      <h1>Papers</h1>
+      {/* Add your papers content here */}
+    </div>
+  );
+};
+
+export default Papers;
